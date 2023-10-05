c707cd7136e67e6530d221de3f2257e86a1c2e2d
vishalkatsa <papajikatsa@gmail.com>
2023-10-05 12:10:37 +0530

    form react app complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import Popup from "./Popup";
 
 export default function Form(props) {
   const [inputName, setinputName] = useState("");
-  const [Mobile, setMobile] = useState('');
+  const [Mobile, setMobile] = useState("");
   const [Email, setEmail] = useState("");
   const [States, setStates] = useState("");
   const [District, setDistrict] = useState("");
@@ -13,11 +13,11 @@ export default function Form(props) {
 
   const [mapData, setMapData] = useState([]);
 
-  const [closes, setcloses] = useState()
-  const [NameValidation, setNameValidation] = useState('black')
+  const [closes, setcloses] = useState();
+  // const [NameValidation, setNameValidation] = useState('black')
   // if(inputName.length > 20){
   //   setNameValidation('red-600')
-    
+
   // }
   const datas = {
     name: inputName,
@@ -28,58 +28,84 @@ export default function Form(props) {
     Village: Village,
   };
 
-  const onsubmit = () => {
-    
+  const onsubmit = (pageNot_reload) => {
+    if (
+      inputName === "" ||
+      Mobile === "" ||
+      Email === "" ||
+      States === "" ||
+      District === "" ||
+      Village === ""
+    ) {
+      alert("Fill The All Box");
+      pageNot_reload.preventDefault();
+    } else if (!/^[a-zA-Z]+$/.test(inputName)) {
+      alert("allow only alphabetic characters in name");
+      pageNot_reload.preventDefault();
+    } else if (inputName.length > 30) {
+      alert("Fill The All Box");
+      pageNot_reload.preventDefault();
+    } else if (!Email.endsWith("@gmail.com")) {
+      alert("Accept Only Gmail");
+      console.log("Email:", Email);
+      pageNot_reload.preventDefault();
+    } else if (States.length > 20) {
+      alert("State name is to long , less than 20 latter");
+      pageNot_reload.preventDefault();
+    } else if (!/^[a-zA-Z]+$/.test(States)) {
+      alert("allow only alphabetic characters in State");
+      pageNot_reload.preventDefault();
+    } else if (District.length > 25) {
+      alert("District name is to long , less then 35 latter");
+      pageNot_reload.preventDefault();
+    } else if (!/^[a-zA-Z]+$/.test(District)) {
+      alert("allow only alphabetic characters in District");
+      pageNot_reload.preventDefault();
+    } else if (Village.length > 25) {
+      alert("Village name is to long , less then 35 latter");
+      pageNot_reload.preventDefault();
+    } else if (!/^[a-zA-Z]+$/.test(Village)) {
+      alert("allow only alphabetic characters in Village");
+      pageNot_reload.preventDefault();
+    } else if (Mobile.length < 10 || Mobile.length > 10) {
+      alert("mobile must be at least 10 characters.");
+      pageNot_reload.preventDefault();
+    } else if (!/^[0-9]+$/.test(Mobile)) {
+      alert("Mobile number must contain only numeric characters.");
+      pageNot_reload.preventDefault();
 
-  if (inputName === '' || Mobile === '' || Email === '' || States === '' || District === '' || Village === '') {
-    alert("Fill The All Box")
-  }
-  else if(inputName.length > 30){
-    alert("Fill The All Box")
-  }
-  else if(Email && Email.endsWith('@gmail.com')){
-    alert('Accept Only Gmail')
-  }
-  
-  else if(States.length > 20){
-    alert('State name is to long , less than 20 latter');
-  }
-  else if(District.length > 25){
-    alert('District name is to long , less then 35 latter')
-  }
-  else if(Village.length > 25){
-    alert('Village name is to long , less then 35 latter')
-  }
-  else if(Mobile.length < 10){
-    alert('mobile must be at least 10 characters.');
-  }
-  else {
-    setSubmits(true);
-    setMapData([...mapData, datas]);
-    console.log(mapData);
-    setcloses('hidden');
-    document.body.style.backgroundColor="rgba(32, 32, 32, 0.5)"
-  }
-};
+    }
+    else {
+      pageNot_reload.preventDefault();
+      setSubmits(true);
+      setMapData([...mapData, datas]);
+      console.log(mapData);
+      setcloses("hidden");
+      document.body.style.backgroundColor = "rgba(32, 32, 32, 0.5)";
+    }
+  };
 
   return (
     <>
-      
       {submits ? <Popup mapData={mapData} /> : ""}
       <div className={`${props.FormBlock} mt-5`}></div>
       <div className={`${closes} w-3/4 mx-auto`}>
-        <div className="bg-yellow-400 w-[40%]  mx-auto py-8">
+        <form className="bg-yellow-400 wv mx-auto py-8">
           <label className="block mx-11 font-medium ">Full Name</label>
           <input
-          type="text"  onChange={(e) => {
+            type="text"
+            onChange={(e) => {
               setinputName(e.target.value);
             }}
-            className={`block mx-auto w-[80%]  text-${NameValidation} rounded bg-slate-200 focus:bg-white outline-none px-3 py-3`}
+            className={`block mx-auto w-[80%]  rounded bg-slate-200 focus:bg-white outline-none px-3 py-3`}
           ></input>
 
           <label className="block mx-11 font-medium ">Mobile Number</label>
           <input
-          type="tel" minLength={1} maxLength={10} onChange={(e) => {
+            
+            minLength={1}
+            maxLength={10}
+            onChange={(e) => {
               setMobile(e.target.value);
             }}
             className="block mx-auto w-[80%]  rounded bg-slate-200 focus:bg-white outline-none px-3 py-3"
@@ -87,7 +113,9 @@ export default function Form(props) {
 
           <label className="block mx-11 font-medium ">Email Id</label>
           <input
-          type="email"  onChange={(e) => {
+            type="email"
+            required
+            onChange={(e) => {
               setEmail(e.target.value);
             }}
             className="block mx-auto w-[80%]  rounded bg-slate-200 focus:bg-white outline-none px-3 py-3"
@@ -95,7 +123,8 @@ export default function Form(props) {
 
           <label className="block mx-11 font-medium ">State</label>
           <input
-          type="text"  onChange={(e) => {
+            type="text"
+            onChange={(e) => {
               setStates(e.target.value);
             }}
             className="block mx-auto w-[80%]  rounded bg-slate-200 focus:bg-white outline-none px-3 py-3"
@@ -103,7 +132,8 @@ export default function Form(props) {
 
           <label className="block mx-11 font-medium ">District</label>
           <input
-          type="text"  onChange={(e) => {
+            type="text"
+            onChange={(e) => {
               setDistrict(e.target.value);
             }}
             className="block mx-auto w-[80%]  rounded bg-slate-200 focus:bg-white outline-none px-3 py-3"
@@ -111,24 +141,29 @@ export default function Form(props) {
 
           <label className="block mx-11 font-medium ">Village</label>
           <input
-          type="text"  onChange={(e) => {
+            type="text"
+            onChange={(e) => {
               setVillage(e.target.value);
             }}
             className="block mx-auto w-[80%]  rounded bg-slate-200 focus:bg-white outline-none px-3 py-3"
           ></input>
-
-          <button
+          <div className="w-36 mx-auto">
+          <input
+            type="submit"
+            onClick={onsubmit}
+            className="py-2 px-10 font-medium  mt-6 bg-green-600 cursor-pointer"
+          /></div>
+          {/* <button
             onClick={onsubmit}
             className="py-2 px-10 font-medium ml-32 mt-6 bg-green-600"
           >
             Submit
-          </button>
-        </div>
+          </button> */}
+        </form>
       </div>
     </>
   );
 }
-
 
 // Form.propTypes = {
 //   inputName: PropTypes.string,
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -10,45 +10,48 @@ export default function Popup(props) {
     <>
       {SecoundAfter ? <Form FormBlock={FormBlock} /> : ""}
 
-      <div className={`${PopupClose} w-[80%] py-4 mt-14 mx-auto h-[500px] bg-zinc-100 vk`}>
+      <div className={`${PopupClose} w-[80%] py-4 mt-14 mx-auto h-[500px] overflow-auto bg-zinc-100 vk`}>
         <div className="flex justify-end  ">
-            <div className=" mr-4 cursor-pointer " onClick={()=>{setPopupClose('hidden'); setFormBlock('block'); setSecoundAfter(true) ; document.body.style.backgroundColor="white"}  }><i  class="fa-solid fa-square-xmark fa-2xl" style={{color : "#ff0000"}}></i></div>
+            <div className=" mr-4 cursor-pointer " onClick={()=>{setPopupClose('hidden'); setFormBlock('block'); setSecoundAfter(true) ; document.body.style.backgroundColor="white"}  }><i  className="fa-solid fa-square-xmark fa-2xl" style={{color : "#ff0000"}}></i></div>
         </div>
-        <h1 className="text-center text-4xl font-bold mt-0 mb-4">Form Data</h1>
-        <table className="bg-zinc-300 w-full mx-auto  ">
+        <h1 className="text-center text-4xl font-bold mt-0 mb-4 ">Form Data</h1>
+        <table className="bg-zinc-300 w-full mx-auto overflow-hidden ">
+          <thead>
           <tr>
-            <th className="px-10 bg-zinc-300"> Sl.No</th>
-            <th className="px-10 bg-zinc-300"> Full Name</th>
-            <th className="px-10 bg-zinc-300"> Mobile Number </th>
-            <th className="px-10 bg-zinc-300"> Email Id </th>
-            <th className="px-10 bg-zinc-300"> State </th>
-            <th className="px-10 bg-zinc-300"> District </th>
-            <th className="px-10 bg-zinc-300"> Village </th>
+            <th className="px-5 bg-zinc-300"> Sl.No</th>
+            <th className="px-5 bg-zinc-300"> Full Name</th>
+            <th className="px-5 bg-zinc-300"> Mobile Number </th>
+            <th className="px-5 bg-zinc-300"> Email Id </th>
+            <th className="px-5 bg-zinc-300"> State </th>
+            <th className="px-5 bg-zinc-300"> District </th>
+            <th className="px-5 bg-zinc-300"> Village </th>
           </tr>
+          </thead>
           {props.mapData.map((data, index) => (
-            <tr key={index}>
-              <td className="px-2 bg-zinc-100 text-center border-blue-700 border-spacing-2">
+            <tbody  key={index}>
+            <tr>
+              <td className="px-5 bg-zinc-100 text-center border-blue-700 border-spacing-2">
                 {index + 1}
               </td>
-              <td className="px-2 bg-zinc-100 text-center border-blue-700 border-spacing-2">
+              <td className="px-5 bg-zinc-100 text-center border-blue-700 border-spacing-2">
                 {data.name}
               </td>
-              <td className="px-2 bg-zinc-100 text-center border-blue-700 border-spacing-2">
+              <td className="px-5 bg-zinc-100 text-center border-blue-700 border-spacing-2">
                 {data.mobile}
               </td>
-              <td className="px-2 bg-zinc-100 text-center border-blue-700 border-spacing-2">
+              <td className="px-5 bg-zinc-100 text-center border-blue-700 border-spacing-2">
                 {data.Email}
               </td>
-              <td className="px-2 bg-zinc-100 text-center border-blue-700 border-spacing-2">
+              <td className="px-5 bg-zinc-100 text-center border-blue-700 border-spacing-2">
                 {data.States}
               </td>
-              <td className="px-2 bg-zinc-100 text-center border-blue-700 border-spacing-2">
+              <td className="px-5 bg-zinc-100 text-center border-blue-700 border-spacing-2">
                 {data.District}
               </td>
-              <td className="px-2 bg-zinc-100 text-center border-blue-700 border-spacing-2">
+              <td className="px-5 bg-zinc-100 text-center border-blue-700 border-spacing-2">
                 {data.Village}
               </td>
-            </tr>
+            </tr></tbody>                
           ))}
         </table>
         <div className="my"></div>
